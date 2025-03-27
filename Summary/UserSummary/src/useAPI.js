a6b3d0b37f8933caafbb72ab9211f8c8aeef54d2
Edanriell <Edanriell@gmail.com>
2025-03-27 13:53:32 +0200

    examples

---
+++ Summary/UserSummary/src/useAPI.js
@@ -0,0 +1,10 @@
+import { useMemo } from "react";
+
+export function useAPI() {
+	return useMemo(
+		() => ({
+			fetchUser: (userId) => fetch(`https://api.company.invalid/users/${userId}`)
+		}),
+		[]
+	);
+}
